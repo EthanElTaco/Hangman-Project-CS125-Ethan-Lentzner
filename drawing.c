9d5895fdc59bcbd08977929ae0afbe734a36268d
EthanElTaco <114040425+EthanElTaco@users.noreply.github.com>
2023-11-14 15:26:05 -0700

    Update drawing.c

--- drawing.c
+++ drawing.c
@@ -2,41 +2,41 @@
 
 void drawing(int* ptrLives) {
 if ((*ptrLives) == 6) {
-printf("You have 6 attempts left\n");
+printf("You have 6 lives left\n");
 printf("  +---+\n  |   | \n      |\n      |\n      |\n      | \n =========\n"); 
 }
 
 
 
 if ((*ptrLives) == 5) {
-printf("You have 5 attempts left\n");
+printf("You have 5 lives left\n");
 printf("  +---+\n  |   | \n  O   |\n      |\n      |\n      | \n =========\n"); 
 }
 
 
 
 if ((*ptrLives) == 4) {
-printf("You have 4 attempts left\n");
+printf("You have 4 lives left\n");
 printf("  +---+\n  |   | \n  O   |\n  |   |\n      |\n      | \n =========\n"); 
 }
 
 
 
 if ((*ptrLives) == 3) {
-printf("You have 3 attempts left\n");
+printf("You have 3 lives left\n");
 printf("  +---+\n  |   | \n  O   |\n /|   |\n      |\n      | \n =========\n"); 
 }
 
 
 
 if ((*ptrLives) == 2) {
-printf("You have 2 attempts left\n");
+printf("You have 2 lives left\n");
 printf("  +---+\n  |   | \n  O   |\n /|\\  |\n      |\n      | \n =========\n"); 
 }
 
 
 if ((*ptrLives) == 1) {
-printf("You have 1 attempt left\n");
+printf("You have 1 life left\n");
 printf("  +---+\n  |   | \n  O   |\n /|\\  |\n /    |\n      | \n =========\n"); 
 }
 
