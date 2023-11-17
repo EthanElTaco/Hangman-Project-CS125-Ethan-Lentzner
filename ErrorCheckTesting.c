e8b0c47fee965d9f7032326cc8dd4574f5bfc225
dmcgoldrick51 <148919417+dmcgoldrick51@users.noreply.github.com>
2023-11-17 11:50:21 -0700

    Update ErrorCheckTesting.c

--- ErrorCheckTesting.c
+++ ErrorCheckTesting.c
@@ -1,53 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int errorCheck(char* ptrguess, char* prevLetter){
-    for (k = 0; k <= strlen(*prevLetter); k++){
-        if (*ptrguess[0] == *prevLetter[k] + 32 || *ptrguess[0] == *prevLetter[k]){
-            printf("This letter has already been entered. Enter new letter: ");
-            fgets(*ptrguess, 16, stdin);
-            return 1;
+    int k;
+    for (k = 0; k <= strlen(prevLetter); k++){
+        while (ptrguess[0] == prevLetter[k] + 32 || ptrguess[0] == prevLetter[k] || (ptrguess[0] < 65 || ptrguess[0] > 122) || (ptrguess[0] > 90 && ptrguess[0] < 97)){
+            printf("Error. Enter new letter: ");
+            fgets(ptrguess, 16, stdin);
+            ptrguess[0] = tolower(ptrguess[0]);
         }
+        return 1;
     }
    return 2; 
 }
 
 
 
+
 //Error Checking
 /*
   char guess[16];
-  char* ptrguess[16] = &guess;
-  char prevLetter[26];
-  char* prevLetter[26] = &prevLetter;
+  char prevLetter[26] = {0};
   char enteredLetter;
   char blanks[10];
+  char alreadyEnt[16];
   int q;
   int j = 0;
   int x;
  */
 /*
     do{
-    if (*ptrguess[0] >= 65 && *ptrguess[0] <= 90){
-        *ptrguess[0] = *ptrguess[0] + 32;
-    }
-    while (*ptrguess[0] < 65 || *ptrguess[0] > 122){
+    while (guess[0] < 65 || guess[0] > 122){
         printf("Not a letter, re-enter letter: ");
-        fgets(*ptrguess, 16, stdin);
-        if (*ptrguess[0] >= 65 && *ptrguess[0] <= 90){
-            *ptrguess[0] = *ptrguess[0] + 32;
+        fgets(guess, 16, stdin);
+        if (guess[0] >= 65 && guess[0] <= 90){
+            guess [0] = tolower(guess[0]);
         }
     }
-    while (*ptrguess[0] > 90 && *ptrguess[0] < 97){
+    while (guess[0] > 90 && guess[0] < 97){
         printf("Not a letter, re-enter letter: ");
-        fgets(*ptrguess, 16, stdin);
-        if (*ptrguess[0] >= 65 && *ptrguess[0] <= 90){
-            *ptrguess[0] = *ptrguess[0] + 32;
+        fgets(guess, 16, stdin);
+        if (guess[0] >= 65 && guess[0] <= 90){
+            guess [0] = tolower(guess[0]);
         }
     }
-    *prevLetter[j] = *ptrguess[0];
     int x = errorCheck(guess, prevLetter);
+    prevLetter[j] = guess[0];
     }while (x == 1);
     j++;
 */
+
+
+
+
+//Add dispaly for guessed letters
